6020b9c0ce80a8bfdc6172ff8fee92637afa8f90
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-02-08 04:56:05 +0600

    all is works now

--- cooking-api.js
+++ cooking-api.js
@@ -11,45 +11,28 @@ const searchingMeal = () => {
         .then(data => collectMeal(data.meals))
         //console.log('name of food ', data.meals[0].strMeal))
         .catch(error => errorHandle())
-
 }
+
 const errorHandle = () => {
     document.getElementById("searchKey").value = '';
     return alert('Nothing is matched!! please search again by another keyword.');
 }
 
-function collectMeal(allMeal) {
-    console.log(allMeal);
+const collectMeal = allMeal => {
     const allMealDiv = document.getElementById('allMeal');
     allMeal.forEach(meal => {
         // console.log('innerlog == ', meal.strMeal);
-        // console.log('image == ', meal.strMealThumb);
-
-
-
-
-        //for (let i = 0; i < allMeal.length; i++) {
-        //const meal = allMeal[i];
         let mealDiv = document.createElement('div');
-        // let h1 = document.createElement('h1');
-        // let h3 = document.createElement('h3');
-
-        // h1.innerText = country.name;
-        // h3.innerText = country.capital;
-        
-
-
         let div = `
-            <img onclick = "displayMealDetails('${meal.strMeal}')" class="image-style" src="${meal.strMealThumb}" alt="">
-            <h1 onclick = "displayMealDetails('${meal.strMeal}')" class = 'meal-name'>${meal.strMeal}</h1>
-            <button onclick = "displayMealDetails('${meal.strMeal}')"><a href="#">Details</a></button>
-        `
-
+            <a href="#">
+                <img onclick = "displayMealDetails('${meal.strMeal}')" class="image-style" src="${meal.strMealThumb}" alt="">
+                <h1 onclick = "displayMealDetails('${meal.strMeal}')" class = 'meal-name'>${meal.strMeal}</h1>
+            </a>
+            `
+        //<button onclick = "displayMealDetails('${meal.strMeal}')">Details</button>
         mealDiv.innerHTML = div;
         mealDiv.className = 'meal';
-
         allMealDiv.appendChild(mealDiv);
-
     });
 }
 
@@ -61,7 +44,6 @@ const displayMealDetails = mealName => {
         .then(data => renderMeal(data.meals))
     //console.log('name of food ', data.meals[0].strMeal))
     //.catch(error => errorHandle())
-
 }
 
 const renderMeal = mealInfo => {
@@ -84,5 +66,5 @@ const renderMeal = mealInfo => {
         <li>${mealInfo[0].strIngredient10}</li>
     </ul>
     `
-    document.getElementById('meal-details').style.display='block';
+    document.getElementById('meal-details').style.display = 'block';
 }
